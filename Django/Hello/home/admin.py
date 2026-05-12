bbddf1dd5840f518ad0fed1e91b74ca1dbcde376
CodeCrafter618 <511198@student.firda.nl>
2026-05-12 09:43:39 +0200

    Add README, result delete route and migration
    Add a comprehensive README for the Valpreventie Risicotest project; introduce a delete_resultaat endpoint and route for results (added import and path in Hello/urls.py) to support deleting results (used for print flow). Also add migration 0006 for model changes, update admin/models/templates, and include updated db.sqlite3 and many autogenerated .pyc files.

--- Django/Hello/home/admin.py
+++ Django/Hello/home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Antwoord, Gemeente, Question, Vragen
+from .models import Antwoord, Gemeente, Question, Resultaat, Vragen
 
 
 @admin.register(Gemeente)
@@ -10,4 +10,22 @@ class GemeenteAdmin(admin.ModelAdmin):
 
 admin.site.register(Question)
 admin.site.register(Vragen)
+
+
+class AntwoordInline(admin.TabularInline):
+	model = Antwoord
+	extra = 0
+	fields = ("vraag", "ja_nee", "behaalde_score", "aangemaakt_op")
+	readonly_fields = ("vraag", "ja_nee", "behaalde_score", "aangemaakt_op")
+	can_delete = False
+
+
+@admin.register(Resultaat)
+class ResultaatAdmin(admin.ModelAdmin):
+	list_display = ("id", "risico", "totale_score", "max_score", "gemeente_zoekterm", "aangemaakt_op")
+	search_fields = ("gemeente_zoekterm", "sessie_key", "risico")
+	list_filter = ("risico", "aangemaakt_op")
+	inlines = [AntwoordInline]
+
+
 admin.site.register(Antwoord)
